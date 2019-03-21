3a212489f4708e48c0a115c48b65bda216a99e62
Ricardo Reis <rickomax@hotmail.com>
2019-03-20 23:17:23 -0300

    Update init.lua

--- lua/entities/csg/init.lua
+++ lua/entities/csg/init.lua
@@ -4,8 +4,10 @@ AddCSLuaFile("shared.lua")
 include "shared.lua"
 
 function ENT:SpawnFunction(ply, tr, ClassName)
-	if (!tr.Hit) then return end
-	local ent = ents.Create(ClassName)
+	if ( !tr.Hit ) then return end
+	local SpawnPos = tr.HitPos + tr.HitNormal * 16
+	local ent = ents.Create( ClassName )
+	ent:SetPos( SpawnPos )
 	ent:Spawn()
 	ent:Activate()
 	return ent
